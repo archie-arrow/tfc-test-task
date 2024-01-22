db3dd5b6635055849cbd908456a2f0ea2efe53ec
Arthur Maydanuk <arthur.maydanuk.dev@gmail.com>
2024-01-22 18:24:58 +0200

    feat: create products-table-component and basic layout, establish routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadComponent: () => import('./components/products-table/products-table.component').then(mod => mod.ProductsTableComponent)
+  },
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'products'
+  },
+];
